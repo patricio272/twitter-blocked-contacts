c9eab0d856c112e0a9f06bd1cd7e0a8865d9dc56
Patricio Toledo <patriciotoledochamorro@gmail.com>
2013-12-30 21:55:56 -0300

    Added correct files Still need to be edited to work and stuff....

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$CONSUMER_KEY='mjqVpd70fJiHEkO8b0ieeA';
+$CONSUMER_SECRET='VOienG5sKO6Dcq3hhoJQUHx9JaRtdttt3Ww1DpP4aoc';
+$OAUTH_CALLBACK='http://patricio272.ciki.me/twitter_blocked_users/oauth.php';
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Get Blocked Contacts</title>
+</head>
+<body>
+	<div align="center">
+		<h2>Get Blocked Contacts</h2>
+
+		Show Blocked Contacts<br><br>
+		<a href="login.php"><img src="./img/blocked_twitter.png"/></a><br>
+		<br><br><br><br><br><br>
+		<div>
+			By<br>
+			Patricio Toledo<br>
+			<a target="_blank" href="http://www.twitter.com/patricio272"><img src="./img/twitter_logo.png" style="width: 50px;height: 50px"/></a><br>
+			<a target="_blank" href="http://www.facebook.com/patricio272"><img src="./img/facebook_logo.png" style="width: 50px;height: 50px"/></a><br>
+		</div>
+	</div>
+</body>
+
+</html>
