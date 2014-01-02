f46d57c2e7ce23eee7a7dceb8aaf2e5ceaada216
Patricio Toledo <patriciotoledochamorro@gmail.com>
2014-01-01 23:37:36 -0300

    saving to DB user data

--- setup/functions.php
+++ setup/functions.php
@@ -12,5 +12,11 @@ require_once('connect.php');
 function save_requester_data($requester_screen_name, $oauth_token, $oauth_token_secret){
     $query = "INSERT INTO requesters (screen_name, oauth_token, oauth_token_secret) values ('" . $requester_screen_name . "', '" . $oauth_token . "', '" . $oauth_token_secret . "');";
     $result = pg_query($GLOBALS['dbconn'], $query);
+    if(!$result){
+        $error= pg_last_error($GLOBALS['dbconn']);
+        if(strstr($error,"duplicate key value")){
+            //this requester has already checked his blocked contacts
+        }
+    }
 }
 ?>
